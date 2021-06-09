cfc7fb6cae3ea21555dc9376df3b6a9b36346667
Drason Chow <emchow@iu.edu>
2021-06-09 15:27:13 -0700

    fixed various error detection bugs and added ability to use any file extension

--- logger.py
+++ logger.py
@@ -3,18 +3,20 @@
 import subprocess
 from shutil import which
 
+import re
 from argparse import ArgumentParser
 from datetime import datetime
-from deserialize import deserialize
+from deserialize import deserialize  # type: ignore
 
 from pathlib import Path
 import json
-import subprocess
 import yaml
 
-import serial
+import serial  # type: ignore
+from serial import SerialException
 
 from typing import Optional
+
 
 class Config:
     port: Optional[str]
@@ -28,104 +30,136 @@ class Config:
     def __init__(self):
         self.port = None
         self.baud = 9600
-        self.name = "out"
+        self.name = "out.csv"
         self.timestamp = True
         self.timeout = 0.1
         self.echo = False
         self.no_handshake = False
-    
+
     def fix(self):
         if self.port == "":
             self.port = None
 
         if self.baud is None:
             self.baud = 9600
-        
+
         if self.name is None or self.name == "":
             self.name = "out"
-        
+
         if self.timestamp is None:
             self.timestamp = False
 
         if self.timeout is None:
             self.timeout = 0.1
-        
+
         if self.echo is None:
             self.echo = False
-        
+
         if self.no_handshake is None:
             self.no_handshake = False
-        
+
         return self
 
 
+def main(
+    port: str,
+    baudrate: int,
+    timeout: float,
+    out: str,
+    echo: bool,
+    handshake: bool
+):
 
-def main(port: str, baudrate: int, timeout: float, out: str, echo: bool, handshake: bool):
+    try:
+        with serial.Serial(port, baudrate, timeout=timeout) as dev:
 
-    with serial.Serial(port, baudrate, timeout=timeout) as dev:
-        
-        print ("Press enter to begin.")
-        input()
+            print("Press enter to begin.")
+            input()
 
-        if handshake:
-            signal = int.to_bytes(0xFF, length=1, byteorder='big')
-    
-            print("Waiting for handshake initiation...")
+            if handshake:
+                signal = int.to_bytes(0xFF, length=1, byteorder='big')
 
-            while dev.read() != signal:
-                pass
+                print("Waiting for handshake initiation...")
 
-            dev.write(signal)
+                i = 0
+                msg = dev.read()
 
-            print("Handshake completed!")
+                while msg != signal:
+                    if not len(msg):
+                        i += 1
+                        print("timed out. attempt {}/3".format(i))
 
-            dev.readline()
+                        if i == 3:
+                            print(
+                                "error: unabled to complete handshake. "
+                                "Did you name a valid port?"
+                            )
+                            exit(1)
 
-        with open(out, 'w') as f:
-            while True:
-                data = dev.readline()
-            
-                # Arduino uses ASCII encoding
-                msg = data[:-2].decode('ascii')
+                    msg = dev.read()
 
-                # Lines marked with ECHO: are treated as debug output.
-                if not msg.startswith("ECHO:"):
-                    if len(data) > 0:  # Valid transission 
-                        # Stop saving 
-                        if msg == "DONE":
-                            break
-                        else:
-                            f.write(msg)
-                            f.write('\n')
-                    
-                        if echo:
-                            print(msg)
-                else:
-                    # 012345  ...       -1
-                    # ECHO:Hello, World!\n
-                    #      ^-----------^ is selected
-                    print(msg[5:].lstrip())
-        
+                dev.write(signal)
+
+                print("Handshake completed!")
+
+                dev.readline()
+
+            with open(out, 'w') as f:
+                while True:
+                    data = dev.readline()
+
+                    # Arduino uses ASCII encoding
+                    msg = data[:-2].decode('ascii')
+
+                    # Lines marked with ECHO: are treated as debug output.
+                    if not msg.startswith("ECHO:"):
+                        if len(data) > 0:  # Valid transission
+                            # Stop saving
+                            if msg == "DONE":
+                                break
+                            else:
+                                f.write(msg)
+                                f.write('\n')
+
+                            if echo:
+                                print(msg)
+                    else:
+                        # 012345  ...       -1
+                        # ECHO:Hello, World!\n
+                        #      ^-----------^ is selected
+                        print(msg[5:].lstrip())
+
+    except SerialException:
+        print("specified port does not name a valid device.")
+
 
 def get_devices():
-    res = subprocess.run(['arduino-cli', 'board', 'list'], stdout=subprocess.PIPE).stdout.decode('utf-8')
-    return [line.split(' ')[0] for line in res.split('\n')[1:-2] if not line.strip().endswith("Unknown")]
+    res = subprocess.run(
+        ['arduino-cli', 'board', 'list'],
+        stdout=subprocess.PIPE
+    ).stdout.decode('utf-8')
+
+    return [
+        line.split(' ')[0] for line in res.split('\n')[1:-2]
+        if not line.strip().endswith("Unknown")
+    ]
+
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-   
+
     parser.add_argument(
         "--port",
-        '-p', 
+        '-p',
         help="Port to listen for communications on",
         type=str,
         default=""
     )
-    
+
     parser.add_argument(
-        "--baud", 
-        help="Baud rate of the communications. By default 9600", 
-        default=-1, 
+        "--baud",
+        help="Baud rate of the communications. By default 9600",
+        default=-1,
         type=int
     )
 
@@ -141,11 +175,11 @@ if __name__ == "__main__":
         help="Timestamp output file",
         action="store_true"
     )
-    
+
     parser.add_argument(
-        "--timeout", 
-        help="Communications timeout in seconds. By default 0.1s", 
-        default=-1, 
+        "--timeout",
+        help="Communications timeout in seconds. By default 0.1s",
+        default=-1,
         type=float
     )
 
@@ -154,7 +188,6 @@ if __name__ == "__main__":
         help="Echo logging data",
         action="store_true"
     )
-
 
     parser.add_argument(
         "--no-handshake",
@@ -184,44 +217,68 @@ if __name__ == "__main__":
                 parser.exit()
         else:
             print("error: config is not a file.")
-            parser.exit() 
+            parser.exit()
     else:
         loaded_args = Config()
 
     if args.port == "":
-            if loaded_args.port is not None:
-                if which('arduino-cli') is not None:
-                    valid_devices = get_devices()
-                    if loaded_args.port in valid_devices:
-                        port = loaded_args.port
-                    else:
-                        print("error: specified port does not name a valid device.")
-                        parser.exit()
+        if loaded_args.port is not None:
+            if which('arduino-cli') is not None:
+                valid_devices = get_devices()
+                if loaded_args.port in valid_devices:
+                    port = loaded_args.port
                 else:
-                    valid_devices = loaded_args.port
-            else:
-                if which('arduino-cli') is not None:
-                    valid_devices = get_devices()
+                    print(
+                        "error: specified port does not name a valid device."
+                    )
 
-                    if len(valid_devices) == 1:
-                        port = valid_devices[0]
-                    else:
-                        print("There are multiple valid devices. Please specify in the config or through the command-line.")
-                else:
-                    parser.print_help()
-                    print("error: arduino-cli not found. You must specify a PORT in the command-line or CONFIG.")
                     parser.exit()
+            else:
+                valid_devices = loaded_args.port
+        else:
+            if which('arduino-cli') is not None:
+                valid_devices = get_devices()
+
+                if len(valid_devices) == 1:
+                    port = valid_devices[0]
+                elif len(valid_devices) > 1:
+                    print(
+                        "error: there are multiple valid devices. "
+                        "Please specify a PORT in the config or through a "
+                        "command-line option. You can list valid devices with"
+                        "`arduino-cli board list'"
+                    )
+
+                    parser.exit()
+
+                elif len(valid_devices) == 0:
+                    print(
+                        "error: there are no valid devices"
+                    )
+
+                    parser.exit()
+            else:
+                parser.print_help()
+                print(
+                    "error: `arduino-cli' not found, cannot determine port"
+                    "automatically. "
+                    "You must specify a PORT in a provided CONFIG or through "
+                    "a command-line option"
+                )
+
+                parser.exit()
     else:
         if which('arduino-cli') is not None:
             valid_devices = get_devices()
             if args.port in valid_devices:
                 port = args.port
-                parser.exit()
             else:
+                port = args.port
                 print("error: specified port does not name a valid device.")
+                parser.exit()
         else:
             port = args.port
-     
+
     if args.baud < 0:
         baud = loaded_args.baud
     else:
@@ -246,16 +303,27 @@ if __name__ == "__main__":
         timestamp = loaded_args.timestamp
     else:
         timestamp = args.timestamp
-    
+
     time = " {:%Y-%m-%d %H.%M.%S}".format(datetime.now()) if timestamp else ""
 
-    if args.name == "":
-        name = loaded_args.name[:-4] if loaded_args.name.endswith('.csv') else loaded_args.name
-        out = "{}{}{}".format(name, time, ".csv")
-    else:
-        name = args.name[:-4] if loaded_args.name.endswith('.csv') else args.name
-        out = "{}{}{}".format(name, time, ".csv")
+    p = re.compile(r"(\w+\.*)\.?(\w+)?")
 
-    #print(port, baud, out, echo, timeout, handshake)
-    
+    if args.name == "":
+        res = p.match(loaded_args.name)
+    else:
+        res = p.match(args.name)
+
+    if res.group(2) is not None:  # type: ignore
+        name = res.group(1)[:-1]  # type: ignore
+        suffix = ".{}".format(res.group(2))  # type: ignore
+    else:
+        name = res.group(1)  # type: ignore
+        suffix = ""
+
+    out = "{}{}{}".format(
+        name,   # type: ignore
+        time,
+        suffix
+    )
+
     main(port, baud, timeout, out, echo, handshake)
